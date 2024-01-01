491c865963622d289c64c50645aafa7038082c9a
Ayush-478 <ayushchauhan2302@gmail.com>
2024-01-01 19:08:48 +0530

    changed aesthetics

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
-//1h20m
+const overlay = document.getElementById('overlay')
+const journal = document.getElementById('journal')
+
+
+
+
+document.addEventListener('click', (e)=>{
+    if (e.target.id =='expand_button'){
+        overlay.classList.add('active');
+        journal.classList.add('active');
+    }
+
+    if (e.target.id == 'overlay'){
+        overlay.classList.remove('active')
+        journal.classList.remove('active')
+    }
+})
